fd0df3ac8610c08834a0ced08bc52faf6a26b9fb
Markus Willmann <markuswillmann87@gmail.com>
2025-04-15 11:55:38 +0200

    added About Me

--- lib/pages/about_me_page.dart
+++ lib/pages/about_me_page.dart
@@ -7,6 +7,23 @@ class AboutMePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hier ist zwei");
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(12, 16, 12, 12),
+        child: Column(
+          spacing: 16,
+          children: [
+            CircleAvatar(
+              radius: 130,
+              backgroundImage: AssetImage("assets/images/kaya.jpeg"),
+            ),
+            Text("Kaya Ranji"),
+            Text("Photograph"),
+            Text(
+                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."),
+          ],
+        ),
+      ),
+    );
   }
 }
